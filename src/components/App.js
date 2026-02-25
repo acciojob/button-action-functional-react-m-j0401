d4561e7f2283df22baaa60ed5faf6cb9d3bc5451
Mehul Jain <mehul.jain2206@gmail.com>
2026-02-25 12:15:00 +0530

    Add button and paragraph to App component for user interaction and feedback

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,8 @@ import "./../styles/App.css";
 function App() {
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click">click</button>
+      <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>
     </div>
   );
 }
